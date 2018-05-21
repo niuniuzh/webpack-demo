b36ea7e6a9b569b478059af2a788589e11e21b29
missnote <niuniu.zh.cn@gmail.com>
2018-05-21 19:59:47 +0800

    entry array

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 module.exports = {
     mode: 'development',
-    entry:'./src/script/main.js',
+    entry:['./src/script/main.js','./src/script/a.js'],
     output:{
         path:path.resolve(__dirname,'dist/js'),
         filename:'bundle.js'
