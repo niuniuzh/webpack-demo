c3b8c03223aa39e8c9a3cde01eb3cffe25ef575a
missnote <niuniu.zh.cn@gmail.com>
2018-05-21 20:34:32 +0800

    plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 var htmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 module.exports = {
-    context:path.resolve(__dirname),
+    context: path.resolve(__dirname),
     mode: 'development',
     entry: {
         main: './src/script/main.js',
         a: './src/script/a.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: '[name]-[chunkhash].js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name]-[chunkhash].js'
     },
     plugins: [
         new htmlWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
+            filename: 'index-[hash].html',
+            inject: 'head'
         })
     ]
 };
