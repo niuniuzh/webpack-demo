015657469fffbf6d3baeb9c7aa9cfba1f917022a
missnote <niuniu.zh.cn@gmail.com>
2018-06-10 09:26:20 +0800

    2018年 06月 10日 星期日 09:26:20 CST

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import layer from './components/layer/layer.js';
+import Layer from './components/layer/layer.js';
 import './components/css/common.css';
 
 const App = function () {
-    const NUM = 1;
-    alert(NUM);
-    console.log(layer);
+    var dom = document.getElementById('app');
+    var layer = new Layer();
+    dom.innerHTML = layer.tpl;
 };
 
 new App();
